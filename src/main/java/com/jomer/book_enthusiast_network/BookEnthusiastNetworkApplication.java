fc06f0a9373ca9b9c0571bf66510a2eaf9d4f986
Jomer Verola <soondevjomer@gmail.com>
2024-08-09 23:29:52 +0800

    Initial commit

---
+++ src/main/java/com/jomer/book_enthusiast_network/BookEnthusiastNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.jomer.book_enthusiast_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEnthusiastNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEnthusiastNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jomer/book_enthusiast_network/BookEnthusiastNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jomer.book_enthusiast_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEnthusiastNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
